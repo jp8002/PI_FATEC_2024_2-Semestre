7268b9699b6d91556408870dc19a003b8894a705
jp8002 <162762832+jp8002@users.noreply.github.com>
2024-10-20 15:50:41 -0300

    Add files via upload

--- sql/almoxarifado.sql
+++ sql/almoxarifado.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13/10/2024 às 16:12
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Generation Time: Oct 20, 2024 at 07:57 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `almoxarifado`
+-- Database: `almoxarifado`
 --
 CREATE DATABASE IF NOT EXISTS `almoxarifado` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `almoxarifado`;
@@ -26,19 +26,32 @@ USE `almoxarifado`;
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `almoxarife`
+-- Table structure for table `almoxarife`
 --
 
 CREATE TABLE `almoxarife` (
   `id` int(10) UNSIGNED NOT NULL,
   `usuario` varchar(45) NOT NULL,
-  `senha` varchar(20) NOT NULL
+  `senha` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `epis`
+-- Table structure for table `devolucao`
+--
+
+CREATE TABLE `devolucao` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `funcionarios_retira_id` int(10) UNSIGNED NOT NULL,
+  `data_entrada` date NOT NULL,
+  `comentario` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `epis`
 --
 
 CREATE TABLE `epis` (
@@ -54,10 +67,10 @@ CREATE TABLE `epis` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `funcionarios_retira_epis`
+-- Table structure for table `funcionarios_retira`
 --
 
-CREATE TABLE `funcionarios_retira_epis` (
+CREATE TABLE `funcionarios_retira` (
   `id` int(10) UNSIGNED NOT NULL,
   `epis_id` int(10) UNSIGNED NOT NULL,
   `almoxarife_id` int(10) UNSIGNED NOT NULL,
@@ -67,61 +80,80 @@ CREATE TABLE `funcionarios_retira_epis` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices de tabela `almoxarife`
+-- Indexes for table `almoxarife`
 --
 ALTER TABLE `almoxarife`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `epis`
+-- Indexes for table `devolucao`
+--
+ALTER TABLE `devolucao`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `devolucao_FKIndex1` (`funcionarios_retira_id`);
+
+--
+-- Indexes for table `epis`
 --
 ALTER TABLE `epis`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `funcionarios_retira_epis`
+-- Indexes for table `funcionarios_retira`
 --
-ALTER TABLE `funcionarios_retira_epis`
-  ADD PRIMARY KEY (`id`),
+ALTER TABLE `funcionarios_retira`
+  ADD PRIMARY KEY (`id`,`epis_id`),
   ADD KEY `funcionarios_has_EPIs_FKIndex2` (`epis_id`),
   ADD KEY `funcionarios_retira_EPIs_FKIndex3` (`almoxarife_id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `almoxarife`
+-- AUTO_INCREMENT for table `almoxarife`
 --
 ALTER TABLE `almoxarife`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `epis`
+-- AUTO_INCREMENT for table `devolucao`
+--
+ALTER TABLE `devolucao`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `epis`
 --
 ALTER TABLE `epis`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `funcionarios_retira_epis`
+-- AUTO_INCREMENT for table `funcionarios_retira`
 --
-ALTER TABLE `funcionarios_retira_epis`
+ALTER TABLE `funcionarios_retira`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- Restrições para tabelas despejadas
+-- Constraints for dumped tables
 --
 
 --
--- Restrições para tabelas `funcionarios_retira_epis`
+-- Constraints for table `devolucao`
 --
-ALTER TABLE `funcionarios_retira_epis`
-  ADD CONSTRAINT `funcionarios_retira_epis_ibfk_1` FOREIGN KEY (`epis_id`) REFERENCES `epis` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `funcionarios_retira_epis_ibfk_2` FOREIGN KEY (`almoxarife_id`) REFERENCES `almoxarife` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `devolucao`
+  ADD CONSTRAINT `devolucao_ibfk_1` FOREIGN KEY (`funcionarios_retira_id`) REFERENCES `funcionarios_retira` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `funcionarios_retira`
+--
+ALTER TABLE `funcionarios_retira`
+  ADD CONSTRAINT `funcionarios_retira_ibfk_1` FOREIGN KEY (`epis_id`) REFERENCES `epis` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `funcionarios_retira_ibfk_2` FOREIGN KEY (`almoxarife_id`) REFERENCES `almoxarife` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
